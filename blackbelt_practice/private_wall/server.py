d09fbb50fa05635371f5c522cd1b7a06629f783b
Austin Chen <114212282+austinchen27@users.noreply.github.com>
2022-11-17 07:02:06 -1000

    Delete blackbelt_practice directory

--- blackbelt_practice/private_wall/server.py
+++
@@ -1,6 +0,0 @@
-from flask_app import app
-from flask_app.controllers import users_controllers, msg_controllers
-
-
-if __name__ == "__main__":
-  app.run(debug=True,port=5005)
--- blackbelt_practice/sas/flask_app/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-app.secret_key = "ch1ck3nandr1c3"
-DATABASE = 'py_exam_schema'
